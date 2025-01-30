c712003ec10ed4273873d5b6704099bb733b43f7
Olino4 <oliver.nedeliak@gmail.com>
2025-01-30 10:31:35 +0100

    update1

--- spotifi/resources/views/home.blade.php
+++ spotifi/resources/views/home.blade.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Spotify Web Playback SDK Quick Start</title>
 </head>
+
 <body>
     <h1>Spotify Web Playback SDK Quick Start</h1>
     <button id="togglePlay">Toggle Play</button>
@@ -10,43 +12,56 @@
     <script src="https://sdk.scdn.co/spotify-player.js"></script>
     <script>
         window.onSpotifyWebPlaybackSDKReady = () => {
-            const token = 'BQBpnY3Rx24mb6loAfVgUmeP2LhdoIB1HaAiTzsbwv44Rd2fCM_a9Cziu7LZFGTlytQvOOo82FV1myV2SJAU32NZ84TGipvEm3S5OjdateZKRT3jSnx5SUQnBJmZgpN5fw1b5TFpkyjo_ssnbiEGsrGcreGy89iV8VMf_x9cGCXWDDboUFTipWboy0JgjmhN5TY0yvVHiaKqF_69EPfZ_GcJ4TkZzVMA7BpDnc-t';
+            const token =
+                'BQBSRSwDF_dAxR4ZvqJl8FMBkPjciPPqPLDUjOFYwx1mk5LCFxmWysYZOUbRHt8oABKrxu_uSYHwZqP7oO3Pxc59b2xA-tLPGqECgG5ntuT0oBz_XOpI00yWunGZlOHIyrIVUchzYRnYLnkwqwZ2iZQ7_TCh4PLn9OF00mLPRTwtXc95h6Yf2044Xhws-lUHcD38TL1sIQwalqu_BgAEtPmbFjZ09ci0oLwqi-AYwlwErrMiqN6hC9FevuVe3DfA';
             const player = new Spotify.Player({
                 name: 'Web Playback SDK Quick Start Player',
-                getOAuthToken: cb => { cb(token); },
+                getOAuthToken: cb => {
+                    cb(token);
+                },
                 volume: 0.5
             });
 
-            if(true){
+            if (true) {
                 console.log(token);
             };
 
             // Ready
-            player.addListener('ready', ({ device_id }) => {
+            player.addListener('ready', ({
+                device_id
+            }) => {
                 console.log('Ready with Device ID', device_id);
             });
 
             // Not Ready
-            player.addListener('not_ready', ({ device_id }) => {
+            player.addListener('not_ready', ({
+                device_id
+            }) => {
                 console.log('Device ID has gone offline', device_id);
             });
 
-            player.addListener('initialization_error', ({ message }) => {
+            player.addListener('initialization_error', ({
+                message
+            }) => {
                 console.error(message);
             });
 
-            player.addListener('authentication_error', ({ message }) => {
+            player.addListener('authentication_error', ({
+                message
+            }) => {
                 console.error(message);
             });
 
-            player.addListener('account_error', ({ message }) => {
+            player.addListener('account_error', ({
+                message
+            }) => {
                 console.error(message);
             });
 
             document.getElementById('togglePlay').onclick = function() {
-              player.togglePlay().then(() => {
-                console.log('Togled play sucesfuly')
-              });
+                player.togglePlay().then(() => {
+                    console.log('Togled play sucesfuly')
+                });
             };
 
             player.connect().then(succes => {
@@ -59,7 +74,5 @@
         }
     </script>
 </body>
+
 </html>
-
-
-

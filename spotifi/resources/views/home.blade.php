63a9956caf797f040720359353165037b4925ad1
Olino4 <oliver.nedeliak@gmail.com>
2025-02-06 09:21:29 +0100

    ikony

--- spotifi/resources/views/home.blade.php
+++ spotifi/resources/views/home.blade.php
@@ -7,13 +7,17 @@
 
 <body>
     <h1>Spotify Web Playback SDK Quick Start</h1>
-    <button id="togglePlay">Toggle Play</button>
+    <div style="display: flex; justify-content: center; top:50% ">
+        <button id="previousTrack">&#8678;</button>
+        <button id="togglePlay">||</button>
+        <button id="nextTrack">&#8680;</button>
 
+    </div>
     <script src="https://sdk.scdn.co/spotify-player.js"></script>
     <script>
         window.onSpotifyWebPlaybackSDKReady = () => {
             const token =
-                'BQBSRSwDF_dAxR4ZvqJl8FMBkPjciPPqPLDUjOFYwx1mk5LCFxmWysYZOUbRHt8oABKrxu_uSYHwZqP7oO3Pxc59b2xA-tLPGqECgG5ntuT0oBz_XOpI00yWunGZlOHIyrIVUchzYRnYLnkwqwZ2iZQ7_TCh4PLn9OF00mLPRTwtXc95h6Yf2044Xhws-lUHcD38TL1sIQwalqu_BgAEtPmbFjZ09ci0oLwqi-AYwlwErrMiqN6hC9FevuVe3DfA';
+                'BQBB5efGXj0lTsrJhWaedRQAqslXT3CPPpHFnRRtq0ZUhwOdX4nsnNG5LoNNKaO9B6soA7s7unSxFdXPurbp9k2Ir7Yw55Dp2GRK6fwH8ilS_Vt8gGOapcbJkLgHD9_BAPf8JrFY1RJhAiiIjygG-O7unDY_U2_NCqm1O50fe24Phv8gqSAnXlzMbk0HEfrdeRpOUONeRPTp7kaMzf7HQqDKqh1J7_py9L6BjmbkJGeIIA5zF4f2DUGr2fLoyfl4';
             const player = new Spotify.Player({
                 name: 'Web Playback SDK Quick Start Player',
                 getOAuthToken: cb => {
@@ -63,6 +67,18 @@
                     console.log('Togled play sucesfuly')
                 });
             };
+
+            document.getElementById('previousTrack').onclick = function() {
+                player.previousTrack().then(() => {
+                    console.log('Set to previous track!');
+                });
+            }
+
+            document.getElementById('nextTrack').onclick = function() {
+                player.nextTrack().then(() => {
+                    console.log('Skipped to next track!');
+                });
+            }
 
             player.connect().then(succes => {
                 if (succes) {
